6f2d86e2d471503225043bdf4ad45977d1f3fe6c
Tatiana <tatumora1302@gmail.com>
2019-04-15 19:14:14 -0600

    Clase Gladiator

---
+++ Pablo/Gladiators/cmake-build-debug/CMakeFiles/Gladiators.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Gladiators.dir/main.cpp.o"
+  "CMakeFiles/Gladiators.dir/Gladiator.cpp.o"
+  "Gladiators.pdb"
+  "Gladiators"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Gladiators.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Pablo/Gladiators/cmake-build-debug/CMakeFiles/Gladiators.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Gladiators.
+# This may be replaced when dependencies are built.
