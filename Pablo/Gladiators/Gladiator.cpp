d9abe7eb929d3f43082911d0fdc5380da71587c5
Tatiana <tatumora1302@gmail.com>
2019-04-15 19:14:05 -0600

    Clase Gladiator

---
+++ Pablo/Gladiators/Gladiator.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tati2327 on 15/04/19.
+//
+
+#include "Gladiator.h"
---
+++ Pablo/Gladiators/Gladiator.h
@@ -0,0 +1,14 @@
+//
+// Created by tati2327 on 15/04/19.
+//
+
+#ifndef GLADIATORS_GLADIATOR_H
+#define GLADIATORS_GLADIATOR_H
+
+
+class Gladiator {
+
+};
+
+
+#endif //GLADIATORS_GLADIATOR_H
